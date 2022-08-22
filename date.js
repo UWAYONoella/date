b70654aedba0903816b2010bcebeb53c745d05f4
UWAYONoella <noellawayo2019@gmail.com>
2022-08-22 21:54:35 +0200

    Feat

---
+++ date.js
@@ -0,0 +1,26 @@
+const today=Date();
+document.write("to day is on " +today);
+
+
+const start = Date.now();
+
+console.log('starting timer...');
+
+setTimeout(() => {
+  const millis = Date.now() - start;
+
+  document.write(`seconds elapsed = ${Math.floor(millis / 1000)}`);
+}, 4000);
+
+// To test a function and get back its return
+// function printElapsedTime(testFn) {
+//     const startTime = Date.now();
+//     const result = testFn();
+//     const endTime = Date.now();
+  
+//     console.log(`Elapsed time: ${String(endTime - startTime)} milliseconds`);
+//     return result;
+//   }
+  
+//   const yourFunctionReturn = printElapsedTime(yourFunction);
+  
